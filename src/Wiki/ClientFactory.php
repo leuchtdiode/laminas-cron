d4075e4d43e05c70c9d3a74a079fef2aaaf436c0
Alex Schloegl <alexander.schloegl@compass.at>
2022-12-19 11:41:16 +0100

    Possibility to add proxy to XMLRPC client (wiki)

--- src/Wiki/ClientFactory.php
+++ src/Wiki/ClientFactory.php
@@ -4,14 +4,21 @@ declare(strict_types=1);
 namespace Cron\Wiki;
 
 use Interop\Container\ContainerInterface;
-use Interop\Container\Exception\ContainerException;
-use Laminas\ServiceManager\Exception\ServiceNotCreatedException;
-use Laminas\ServiceManager\Exception\ServiceNotFoundException;
-use Laminas\ServiceManager\Factory\FactoryInterface;
 use Laminas\Http\Client as HttpClient;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class ClientFactory implements FactoryInterface
 {
+	/**
+	 * @param ContainerInterface $container
+	 * @param $requestedName
+	 * @param array|null $options
+	 * @return Client
+	 * @throws ContainerExceptionInterface
+	 * @throws NotFoundExceptionInterface
+	 */
 	public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
 	{
 		$config = $container->get('config')['cron']['wiki'];
@@ -21,6 +28,19 @@ class ClientFactory implements FactoryInterface
 		if (($user = $config['user'] ?? null) && ($password = $config['password']))
 		{
 			$httpClient->setAuth($user, $password);
+		}
+
+		if (($proxy = $config['proxy'] ?? null))
+		{
+			$proxyAdapter = new HttpClient\Adapter\Proxy();
+			$proxyAdapter->setOptions([
+				'proxy_host' => $proxy['host'],
+				'proxy_port' => $proxy['port'],
+				'proxy_user' => $proxy['user'] ?? '',
+				'proxy_pass' => $proxy['password'] ?? '',
+			]);
+
+			$httpClient->setAdapter($proxyAdapter);
 		}
 
 		return new Client($config['host'], $httpClient);
